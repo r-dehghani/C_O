fc5f012f8b26f114ffc2e28df66cca1b614a191a
Dariush Dehghani <r.dehghani.90@gmail.com>
2021-07-08 14:58:42 +0430

    2- create docker-compose file and run two simple services a: api b: postgresql DB

--- C_O_API_Project/settings.py
+++ C_O_API_Project/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'C_O_API_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'dariush',
+        'HOST': 'db',
+        'PORT': 5432 ,
     }
 }
 
